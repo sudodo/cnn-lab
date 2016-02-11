e3418b8428898c45b5f6d310bd2f1519362afd8a
ttlg <ttlgftrol@gmail.com>
2016-02-11 18:04:24 +0900

    optimizers

--- make_graph.py
+++ make_graph.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def averaging(seq, sigma=0.01):
+def averaging(seq, sigma=0):
     from scipy.ndimage import gaussian_filter
     return gaussian_filter(seq, sigma=sigma)
 
@@ -22,18 +22,52 @@ def make_graph(acc_path, loss_path, xlim=300):
     plt.show()
 
 
-def make_graph_cmp(*kwargs):
+def make_graph_cmp(title, sigma, *kwargs):
+
+    std = []
+    max = []
     for path, label in kwargs:
         acc = np.loadtxt(path)
-        acc = averaging(acc)
+        std.append([label, np.std(acc)])
+#        print label + ": " + str(np.std(acc))
+        acc = averaging(acc, sigma)
+        max.append([label, np.max(acc)])
+        print label + ": " + str(np.max(acc))
         plt.plot(acc, label=label)
     plt.xlabel('epoch')
     plt.ylabel('accuracy')
-    plt.ylim([0.8, 1])
+    plt.ylim([0.98, 0.994])
     plt.legend(loc='lower right')
+    plt.title(title)
     plt.grid()
     plt.show()
 
+    plt.bar(range(len(std)), map(float, np.array(std)[:, 1].tolist()), align='center')
+    plt.xticks(range(len(std)), np.array(std)[:, 0])
+    plt.title('standard deviation')
+    plt.show()
+
+    plt.bar(range(len(max)), map(float, np.array(max)[:, 1].tolist()), align='center')
+    plt.xticks(range(len(max)), np.array(max)[:, 0])
+    plt.title('max accuracy')
+    plt.ylim([0.985, 0.994])
+    plt.show()
+
+def best_optimizer():
+    make_graph_cmp(
+        "which is the best optimizer[sigma=0]",
+        0,
+        ('results/cnn_SGD_accuracy.txt', 'SGD'),
+        ('results/cnn_MomentumSGD_accuracy.txt', 'MomentumSGD'),
+        ('results/cnn_NesterovAG_accuracy.txt', 'NesterovAG'),
+        ('results/cnn_RMSpropGraves_accuracy.txt', 'RMSpropGraves'),
+        ('results/cnn_AdaDelta_accuracy.txt', 'AdaDelta'),
+        ('results/cnn_AdaGrad_accuracy.txt', 'AdaGrad'),
+        ('results/cnn_Adam_accuracy.txt', 'Adam')
+                   )
+
 
 if __name__ == '__main__':
-    make_graph('results/mlp_accuracy.txt', 'results/mlp_accuracy_train.txt')
+    best_optimizer()
+#    make_graph('results/mlp_accuracy.txt', 'results/mlp_accuracy_train.txt')
+
--- validation.py
+++ validation.py
@@ -5,7 +5,7 @@ from data import MNIST
 import copy
 
 
-def fold_i(model, optimizer, x_train, x_test, t_train, t_test, batch, n_epoch):
+def validate_model(model, optimizer, x_train, x_test, t_train, t_test, batch, n_epoch):
     train_size = t_train.size
     test_size = t_test.size
     print train_size, test_size
@@ -49,7 +49,7 @@ def save_result(path, data):
     np.savetxt(path, data)
 
 
-def k_fold_validation(k, model, tag='', optimizer=optimizers.Adam(), batch=100, n_epoch=100):
+def k_fold_validation(k, model, optimizer=optimizers.Adam(), tag='', batch=100, n_epoch=300):
     loss = []
     acc = []
     acc_train = []
@@ -58,19 +58,30 @@ def k_fold_validation(k, model, tag='', optimizer=optimizers.Adam(), batch=100, 
     for i in range(k):
         x_train, x_test, t_train, t_test = mnist.get_fold_i(k, i)
         print 'fold:' + str(i)
-        l, at, a = fold_i(copy.deepcopy(model),
-                          copy.deepcopy(optimizer),
-                          x_train, x_test, t_train, t_test,
-                          batch, n_epoch)
+        l, at, a = validate_model(copy.deepcopy(model), copy.deepcopy(optimizer),
+                                  x_train, x_test, t_train, t_test,
+                                  batch, n_epoch)
         loss.append(l)
         acc.append(a)
         acc_train.append(at)
-    save_result('results/'+str(model)+tag+'_loss.txt', loss)
-    save_result('results/'+str(model)+tag+'_accuracy.txt', acc)
-    save_result('results/'+str(model)+tag+'_accuracy_train.txt', acc_train)
+    save_result('results/'+str(model)+'_'+tag+'_loss.txt', loss)
+    save_result('results/'+str(model)+'_'+tag+'_accuracy.txt', acc)
+    save_result('results/'+str(model)+'_'+tag+'_accuracy_train.txt', acc_train)
+
+
+def which_is_best_optimizer(k=10, model=CNN()):
+#    k_fold_validation(k, copy.deepcopy(model), optimizer=optimizers.Adam(), tag='Adam')
+#    k_fold_validation(k, copy.deepcopy(model), optimizer=optimizers.SGD(), tag='SGD')
+#    k_fold_validation(k, copy.deepcopy(model), optimizer=optimizers.RMSpropGraves(), tag='RMSpropGraves')
+    k_fold_validation(k, copy.deepcopy(model), optimizer=optimizers.RMSprop(), tag='RMSprop')
+#    k_fold_validation(k, copy.deepcopy(model), optimizer=optimizers.AdaDelta(), tag='AdaDelta')
+#    k_fold_validation(k, copy.deepcopy(model), optimizer=optimizers.AdaGrad(), tag='AdaGrad')
+#    k_fold_validation(k, copy.deepcopy(model), optimizer=optimizers.MomentumSGD(), tag='MomentumSGD')
+#    k_fold_validation(k, copy.deepcopy(model), optimizer=optimizers.NesterovAG(), tag='NesterovAG')
+
 
 if __name__ == '__main__':
-    #k_fold_validation(10, MLP())
-    k_fold_validation(10, CNN())
+    which_is_best_optimizer(model=CNN())
+
 
 
